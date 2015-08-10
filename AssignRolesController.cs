66fd9884a64318a4b01693f35ab8fb4c9d3a8c5f
jnanderson <jnanderson@unomaha.edu>
2015-08-09 19:20:52 -0500

    First commit

---
+++ AssignRolesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdaptiveLearningFinal.Models;
+
+namespace AdaptiveLearningFinal.Controllers
+{ 
+    public class AssignRolesController : Controller
+    {
+        private AdaptiveLearningEntities db = new AdaptiveLearningEntities();
+
+        //
+        // GET: /AssignRoles/
+       [Authorize(Roles = "Administrator")]
+        public ViewResult Index()
+        {
+            var usersinroles = db.UsersInRoles.Include(u => u.Role).Include(u => u.User);
+            return View(usersinroles.ToList());
+        }
+
+        //
+        // GET: /AssignRoles/Details/5
+        [Authorize(Roles = "Administrator")]
+        public ViewResult Details(int id)
+        {
+            UsersInRole usersinrole = db.UsersInRoles.Find(id);
+            return View(usersinrole);
+        }
+
+        //
+        // GET: /AssignRoles/Create
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Create()
+        {
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        } 
+
+        //
+        // POST: /AssignRoles/Create
+
+        [HttpPost, Authorize(Roles = "Administrator")]
+        public ActionResult Create(UsersInRole usersinrole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UsersInRoles.Add(usersinrole);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName", usersinrole.RoleId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", usersinrole.UserId);
+            return View(usersinrole);
+        }
+        
+        //
+        // GET: /AssignRoles/Edit/5
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Edit(int id)
+        {
+            UsersInRole usersinrole = db.UsersInRoles.Find(id);
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName", usersinrole.RoleId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", usersinrole.UserId);
+            return View(usersinrole);
+        }
+
+        //
+        // POST: /AssignRoles/Edit/5
+
+        [HttpPost, Authorize(Roles = "Administrator")]
+        public ActionResult Edit(UsersInRole usersinrole)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usersinrole).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName", usersinrole.RoleId);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", usersinrole.UserId);
+            return View(usersinrole);
+        }
+
+        //
+        // GET: /AssignRoles/Delete/5
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Delete(int id)
+        {
+            UsersInRole usersinrole = db.UsersInRoles.Find(id);
+            return View(usersinrole);
+        }
+
+        //
+        // POST: /AssignRoles/Delete/5
+
+        [HttpPost, ActionName("Delete"), Authorize(Roles = "Administrator")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            UsersInRole usersinrole = db.UsersInRoles.Find(id);
+            db.UsersInRoles.Remove(usersinrole);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ResultsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AdaptiveLearningFinal.Models;
+
+namespace AdaptiveLearningFinal.Controllers
+{ 
+    public class ResultsController : Controller
+    {
+        private AdaptiveLearningEntities db = new AdaptiveLearningEntities();
+
+        //
+        // GET: /Results/
+
+        public ViewResult Index()
+        {
+            var courseresults = db.CourseResults.Include(c => c.CourseQuestion).Include(c => c.User);
+            return View(courseresults.ToList());
+        }
+
+        //
+        // GET: /Results/Details/5
+
+        public ViewResult Details(int id)
+        {
+            CourseResult courseresult = db.CourseResults.Find(id);
+            return View(courseresult);
+        }
+
+        //
+        // GET: /Results/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.QuestionID = new SelectList(db.CourseQuestions, "QuestionID", "QuestionAnswer");
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName");
+            return View();
+        } 
+
+        //
+        // POST: /Results/Create
+
+        [HttpPost]
+        public ActionResult Create(CourseResult courseresult)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CourseResults.Add(courseresult);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.QuestionID = new SelectList(db.CourseQuestions, "QuestionID", "QuestionAnswer", courseresult.QuestionID);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", courseresult.UserId);
+            return View(courseresult);
+        }
+        
+        //
+        // GET: /Results/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            CourseResult courseresult = db.CourseResults.Find(id);
+            ViewBag.QuestionID = new SelectList(db.CourseQuestions, "QuestionID", "QuestionAnswer", courseresult.QuestionID);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", courseresult.UserId);
+            return View(courseresult);
+        }
+
+        //
+        // POST: /Results/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(CourseResult courseresult)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(courseresult).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.QuestionID = new SelectList(db.CourseQuestions, "QuestionID", "QuestionAnswer", courseresult.QuestionID);
+            ViewBag.UserId = new SelectList(db.Users, "UserId", "UserName", courseresult.UserId);
+            return View(courseresult);
+        }
+
+        //
+        // GET: /Results/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            CourseResult courseresult = db.CourseResults.Find(id);
+            return View(courseresult);
+        }
+
+        //
+        // POST: /Results/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {            
+            CourseResult courseresult = db.CourseResults.Find(id);
+            db.CourseResults.Remove(courseresult);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
